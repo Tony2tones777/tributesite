9204262f6944c9aa80fcc4b94fae9a320cf8b921
Anthony <anthony.marque@gmail.com>
2017-10-10 18:34:32 +0200

    just did my first assignment

---
+++ angular/basics-components/src/app/warning/warning.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning',
+  templateUrl: './warning.component.html',
+  styles: [`
+    .danger {
+      background-color: red;
+    }
+  `]
+})
+export class WarningComponent {
+
+}
