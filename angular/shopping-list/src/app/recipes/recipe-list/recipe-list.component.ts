a1ceab773af2ac937adf3a922805a55607e4a921
Anthony <anthony.marque@gmail.com>
2017-10-19 21:22:19 +0200

    added some components and made an array with a recipe in it

--- angular/shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular/shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import {  Recipe  } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe ('A test recipe', ' this is still just a test', 'http://img-aws.ehowcdn.com/600x600p/photos.demandstudios.com/65/63/fotolia_4098846_XS.jpg')
+  ];
 
   constructor() { }
 
