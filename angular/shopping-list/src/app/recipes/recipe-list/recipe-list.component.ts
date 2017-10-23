f798e876cc8a98d39a0b68c5c4cee2c14fece7a9
Anthony <anthony.marque@gmail.com>
2017-10-23 17:28:03 +0200

    finishing listing components

--- angular/shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular/shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,8 +9,10 @@ import {  Recipe  } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
+    new Recipe ('A test recipe', ' this is still just a test', 'http://img-aws.ehowcdn.com/600x600p/photos.demandstudios.com/65/63/fotolia_4098846_XS.jpg'),
     new Recipe ('A test recipe', ' this is still just a test', 'http://img-aws.ehowcdn.com/600x600p/photos.demandstudios.com/65/63/fotolia_4098846_XS.jpg')
   ];
+
 
   constructor() { }
 
